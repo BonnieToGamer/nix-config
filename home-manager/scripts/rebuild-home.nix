58a9d5a944705ba33a87e618a908ac577fc1d70e
BonnieToGamer <68508540+BonnieToGamer@users.noreply.github.com>
2024-05-13 17:14:03 +0200

    home-manager 2024-05-13 17:09 : id 39

---
+++ home-manager/scripts/rebuild-home.nix
@@ -0,0 +1,29 @@
+{ pkgs }: 
+pkgs.writeShellScriptBin "rebuild-home" ''
+
+set -e
+
+pushd ~/nix
+
+if ${pkgs.git}/bin/git diff --quiet './home-manager/*.nix'; then
+	echo "No changes detected, exiting."
+	popd
+	exit 0
+fi
+
+${pkgs.git}/bin/git diff -U0 '*.nix'
+${pkgs.git}/bin/git add .
+
+echo "Rebuilding home-manager"
+
+home-manager switch --flake ~/nix &>~/.home-manager-rebuild.log || (cat ~/.home-manager-rebuild.log | grep --color error && exit 1)
+
+commitMessage="home-manager $(home-manager generations | head -n 1 | cut -d ' ' -f 1-5)"
+
+${pkgs.git}/bin/git reset ./nixos
+git commit -am "$commitMessage"
+
+popd
+
+${pkgs.libnotify}/bin/notify-send -e "Rebuilt home-manager OK!" --icon=software-update-available
+''
