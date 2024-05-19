d4fd304d4671ab73915d31aa08c9f325457d2e7a
BonnieToGamer <68508540+BonnieToGamer@users.noreply.github.com>
2024-05-19 13:16:08 +0200

    76 current  2024-05-19 13:16:01  23.11.20240511.71bae31   6.1.90                          *

--- nixos/modules/steam.nix
+++ nixos/modules/steam.nix
@@ -1,15 +1,36 @@
 { pkgs, ... }: {
-	programs.steam.enable = true;
-	programs.steam.gamescopeSession.enable = true;
+	programs.steam = {
+		enable = true;
+		gamescopeSession.enable = true;
+	};
 
-	environment.systemPackages = with pkgs; [
-		mangohud
-		protonup
-	];
+	environment = {
+		systemPackages = with pkgs; [
+			mangohud
+			protonup
+		];
 
-	environment.sessionVariables = {
-		STEAM_EXTRA_COMPAT_TOOLS_PATHS = "/home/filip/.steam/root/compatibilitytools.d";
+		sessionVariables = {
+			STEAM_EXTRA_COMPAT_TOOLS_PATHS = "/home/filip/.steam/root/compatibilitytools.d";
+		};
 	};
 
 	programs.gamemode.enable = true;
+
+	nixpkgs.config.packageOverrides = pkgs: {
+		steam = pkgs.steam.override {
+			extraPkgs = pkgs: with pkgs; [
+				xorg.libXcursor
+				xorg.libXi
+				xorg.libXinerama
+				xorg.libXScrnSaver
+				libpng
+				libpulseaudio
+				libvorbis
+				stdenv.cc.cc.lib
+				libkrb5
+				keyutils
+			];
+		};
+	};
 }
