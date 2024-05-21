d6a07b2df8add19c6523dacac06fa387d6f98144
BonnieToGamer <68508540+BonnieToGamer@users.noreply.github.com>
2024-05-21 21:07:32 +0200

    84 current  2024-05-21 20:46:38  23.11.20240511.71bae31   6.1.90                          *

--- nixos/modules/stylix.nix
+++ nixos/modules/stylix.nix
@@ -1,8 +1,8 @@
 { pkgs, lib, ... }: {
-	stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
-	
-	stylix.image = lib.stylix.types.wallpaper.from.image {
-		file = ../themes/hesita-nix.png;
-		polarity = "dark";
+	stylix = {
+		base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
+		image = ../themes/hestia-nix.png;
+
+		autoEnable = true;
 	};
 }
