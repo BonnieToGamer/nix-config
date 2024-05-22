8ac4974a056b4a5c23f913504a19392baf00c497
BonnieToGamer <68508540+BonnieToGamer@users.noreply.github.com>
2024-05-22 21:13:08 +0200

    94 current  2024-05-22 21:13:00  23.11.20240511.71bae31   6.1.90                          *

--- nixos/modules/bluetooth.nix
+++ nixos/modules/bluetooth.nix
@@ -1,12 +1,19 @@
 { pkgs, ... }: {
 	hardware.bluetooth = {
 		enable = true;
+		powerOnBoot = true;
 		package = pkgs.bluez;
 		settings = {
 			General = {
 				ControllerMode = "dual";
 				Enable = "Source,Sink,Media,HID";
 				Class = "0x000540";
+			};
+		};
+
+		input = {
+			General = {
+				UserspaceHID = true;
 			};
 		};
 	};
