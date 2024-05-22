243a56881cbeb20f862124c279a727c2d2cd27d4
BonnieToGamer <68508540+BonnieToGamer@users.noreply.github.com>
2024-05-22 13:10:27 +0200

    87 current  2024-05-22 13:10:20  23.11.20240511.71bae31   6.1.90                          *

--- nixos/modules/sddm.nix
+++ nixos/modules/sddm.nix
@@ -4,7 +4,7 @@
 		displayManager = {
 			sddm.enable = true;
 			sddm.theme = "${import ../pkgs/sddm-flower-theme.nix { inherit pkgs; }}";
-			setupCommands = "${pkgs.xorg.xrandr}/bin/xrandr --output HDMI-A-0 --off &>> /home/filip/xrandr-out.log";
+			setupCommands = "${pkgs.xorg.xrandr}/bin/xrandr --output HDMI-A-0 --off ";
 		};
 	};
 }
