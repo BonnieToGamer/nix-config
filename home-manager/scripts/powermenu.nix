708e1cf276e14c993512813778e815284625421d
BonnieToGamer <68508540+BonnieToGamer@users.noreply.github.com>
2024-05-22 23:38:18 +0200

    home-manager 2024-05-22 23:38 : id 151

--- home-manager/scripts/powermenu.nix
+++ home-manager/scripts/powermenu.nix
@@ -9,7 +9,6 @@ ${pkgs.rofi}/bin/rofi \
 	-show power-menu \
 	-modi power-menu:'rofi-power-menu --symbols-font "Symbols Nerd Font Mono" --choices=shutdown/reboot/lockscreen' \
 	-font "Jetbrains Mono NF 16" \
-	-theme Paper \
-	-theme-str 'window {width: 8em;} listview {lines: 6;}'
+	-theme-str 'window {width: 16em;} listview {lines: 3;}'
 
 ''
