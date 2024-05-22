62241752c3c18ece43a9fb013d4abfbaf0a1ca47
BonnieToGamer <68508540+BonnieToGamer@users.noreply.github.com>
2024-05-22 20:16:35 +0200

    91 current  2024-05-22 20:16:28  23.11.20240511.71bae31   6.1.90                          *

--- nixos/modules/bootloader.nix
+++ nixos/modules/bootloader.nix
@@ -6,6 +6,7 @@
 	
 	boot.initrd.kernelModules = [ "amdgpu" "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
 	boot.kernelParams = [ "nvidia-drm.modeset=1" ];
+	boot.extraModprobeConfig = '' options bluetooth disable_ertm=1 '';
 	
 	boot.supportedFilesystems = [ "ntfs" ];
 }
