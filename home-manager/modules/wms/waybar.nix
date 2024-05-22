0d8ce0d943da3737e95e5ce880a2d0b0d4e1abd4
BonnieToGamer <68508540+BonnieToGamer@users.noreply.github.com>
2024-05-22 11:54:33 +0200

    home-manager 2024-05-22 11:48 : id 144

--- home-manager/modules/wms/waybar.nix
+++ home-manager/modules/wms/waybar.nix
@@ -99,6 +99,6 @@
 			};
 		};
 
-		style = "${builtins.replaceStrings [ "$$base00$$" "$$base01$$" "$$base02$$" "$$base03$$" "$$base04$$" "$$base05$$" "$$base06$$" "$$base07$$" "$$base08$$" "$$base09$$" "$$base0A$$" "$$base0B$$" "$$base0C$$" "$$base0D$$" "$$base0E$$"  ] [ config.colorScheme.colors.base00 config.colorScheme.colors.base01 config.colorScheme.colors.base02 config.colorScheme.colors.base03 config.colorScheme.colors.base04 config.colorScheme.colors.base05 config.colorScheme.colors.base06 config.colorScheme.colors.base07 config.colorScheme.colors.base08 config.colorScheme.colors.base09 config.colorScheme.colors.base0A config.colorScheme.colors.base0B config.colorScheme.colors.base0C config.colorScheme.colors.base0D config.colorScheme.colors.base0E ] css_file}"; 
+		style = with config.colorScheme.colors; "${builtins.replaceStrings [ "$$base00$$" "$$base01$$" "$$base02$$" "$$base03$$" "$$base04$$" "$$base05$$" "$$base06$$" "$$base07$$" "$$base08$$" "$$base09$$" "$$base0A$$" "$$base0B$$" "$$base0C$$" "$$base0D$$" "$$base0E$$"  ] [ base00 base01 base02 base03 base04 base05 base06 base07 base08 base09 base0A base0B base0C base0D base0E ] css_file}"; 
 	};
 }
