df323dae9dd58fe96e7865bdf359cfdd8a97b924
BonnieToGamer <68508540+BonnieToGamer@users.noreply.github.com>
2024-05-22 20:45:17 +0200

    92 current  2024-05-22 20:45:09  23.11.20240511.71bae31   6.1.90                          *

--- nixos/modules/bluetooth.nix
+++ nixos/modules/bluetooth.nix
@@ -1,5 +1,15 @@
 { pkgs, ... }: {
-	hardware.bluetooth.enable = true;
-	hardware.bluetooth.package = pkgs.bluez;
+	hardware.bluetooth = {
+		enable = true;
+		package = pkgs.bluez;
+		settings = {
+			General = {
+				ControllerMode = "dual";
+				Enable = "Source,Sink,Media,HID";
+				Class = "0x000540";
+			};
+		};
+	};
+
 	services.blueman.enable = true;
 }
