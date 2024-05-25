99ad508fd84ee6390b7c0d75e7f50e527f171c28
BonnieToGamer <68508540+BonnieToGamer@users.noreply.github.com>
2024-05-25 16:13:17 +0200

    102 current  2024-05-25 16:13:08  23.11.20240511.71bae31   6.1.90                          *

--- nixos/modules/hamachi.nix
+++ nixos/modules/hamachi.nix
@@ -1,3 +1,8 @@
-{
-	programs.haguichi.enable = true;
+{ lib, ... }: {
+	config.programs.haguichi.enable = true;
+	
+	config.system.activationScripts.makeHamachiConfig = lib.stringAfter [ "var" ] ''
+mkdir -p /var/lib/logmein-hamachi
+echo "Ipc.User filip" > /var/lib/logmein-hamachi/h2-engine-override.cfg
+	'';
 }
