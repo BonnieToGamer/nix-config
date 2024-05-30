492e45c70f5999c7baaffb9382d633cd0eddbaaa
BonnieToGamer <68508540+BonnieToGamer@users.noreply.github.com>
2024-05-30 10:39:09 +0200

    106 current  2024-05-30 10:39:01  23.11.20240511.71bae31   6.1.90                          *

--- nixos/modules/bootloader.nix
+++ nixos/modules/bootloader.nix
@@ -4,7 +4,7 @@
 		efi.canTouchEfiVariables = true;
 	};
 	
-	boot.initrd.kernelModules = [ "amdgpu" "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
+	boot.initrd.kernelModules = [ "nvidia" "nvidia_modeset" "nvidia_uvm" "nvidia_drm" ];
 	boot.kernelModules = [ "hidp" ];
 	boot.kernelParams = [ "nvidia-drm.modeset=1" ];
 	boot.extraModprobeConfig = '' options bluetooth disable_ertm=1 '';
