cf7fe726fbc30a2977611ea4312c7e2fe46cc57a
BonnieToGamer <68508540+BonnieToGamer@users.noreply.github.com>
2024-06-01 01:18:01 +0200

    home-manager 2024-05-31 17:51 : id 164

--- home-manager/modules/xdg.nix
+++ home-manager/modules/xdg.nix
@@ -1,12 +1,18 @@
 {
 	xdg = {
 		mimeApps.defaultApplications = {
-			"text/plain" = [ "neovide.desktop" ];
+			"text/plain" = [ "NotepadNext.desktop" ];
 			"application/pdf" = [ "zathura.desktop" ];
 			"image/*" = [ "sxiv.desktop" ];
-			"video/png" = [ "mpv.desktop" ];
-			"video/jpg" = [ "mpv.desktop" ];
-			"video/*" = [ "mpv.desktop" ];
+			"video/*" = [ "mpv.desktop" "vlc.desktop" ];
+			"application/vnd.rar" = "org.gnome.FileRoller.desktop";
+			"application/x-matroska" = "mpv.desktop";
+			"application/x-xpinstall" = "org.gnome.FileRoller.desktop";
+			"application/zip" = "org.gnome.FileRoller.desktop";
+			"text/*" = [ "NotepadNext.desktop" "code.desktop" ];
+			"x-scheme-handler/http" = "firefox.desktop";
+			"x-scheme-handler/https" = "firefox.desktop";
+			"x-scheme-handler/mailto" = "firefox.desktop";
 		};
 	};
 }
