020eabbb755dab12174cca9a550209d70c383beb
BonnieToGamer <68508540+BonnieToGamer@users.noreply.github.com>
2024-06-01 13:54:15 +0200

    128 current  2024-06-01 13:54:06  23.11.20240511.71bae31   6.1.90                          *

--- nixos/modules/sddm.nix
+++ nixos/modules/sddm.nix
@@ -4,7 +4,7 @@
 		displayManager = {
 			sddm.enable = true;
 			sddm.theme = "${import ../pkgs/sddm-flower-theme.nix { inherit pkgs; }}";
-			setupCommands = "echo test >> /home/filip/wlrrandr.log; wlr-randr --output HDMI-A-1 --off &>> /home/filip/wlrrandr.log";
+			setupCommands = "echo test >> /home/filip/wlrrandr.log; ${pkgs.wlr-randr}/bin/wlr-randr --output HDMI-A-1 --off &>> /home/filip/wlrrandr.log";
 		};
 	};
 }
