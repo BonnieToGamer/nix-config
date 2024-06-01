843112780dd5fa6e4aa2e1b7c43af6b909b9bbf9
BonnieToGamer <68508540+BonnieToGamer@users.noreply.github.com>
2024-06-01 13:52:04 +0200

    127 current  2024-06-01 13:51:54  23.11.20240511.71bae31   6.1.90                          *

--- nixos/modules/sddm.nix
+++ nixos/modules/sddm.nix
@@ -3,7 +3,6 @@
 		enable = true;
 		displayManager = {
 			sddm.enable = true;
-			sddm.wayland.enable = true;
 			sddm.theme = "${import ../pkgs/sddm-flower-theme.nix { inherit pkgs; }}";
 			setupCommands = "echo test >> /home/filip/wlrrandr.log; wlr-randr --output HDMI-A-1 --off &>> /home/filip/wlrrandr.log";
 		};
