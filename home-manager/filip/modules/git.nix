714d4c6a602a906db5bf3ffbc80f89b50fe52893
BonnieToGamer <68508540+BonnieToGamer@users.noreply.github.com>
2024-06-02 20:30:58 +0200

    fixing git

--- home-manager/filip/modules/git.nix
+++ home-manager/filip/modules/git.nix
@@ -3,5 +3,7 @@
     enable = true;
     userName = "BonnieToGamer";
     userEmail = "68508540+BonnieToGamer@users.noreply.github.com";
+
+    extraConfig.core.sshCommand = "ssh -i ~/.sshid_ed25519_bonnie";
   };
 }
