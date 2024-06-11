ce521d8997bc96ad2ec3de5a9eae00bbf1064082
BonnieToGamer <68508540+BonnieToGamer@users.noreply.github.com>
2024-06-11 22:30:37 +0200

    home-manager 2024-06-11 22:30 : id 227

--- home-manager/filip/derivations/moonscraper.nix
+++ home-manager/filip/derivations/moonscraper.nix
@@ -10,7 +10,7 @@ pkgs.stdenv.mkDerivation {
     mkdir -p $out/bin/
     mkdir -p $out/share/
 
-    install -Dm "Moonscraper Chart Editor.x86_64" "$out/bin/Moonscraper Chart Editor.x86_64"
+    install -Dm755 "Moonscraper Chart Editor.x86_64" "$out/bin/Moonscraper Chart Editor.x86_64"
     cp -r "Moonscraper Chart Editor_Data" "$out/share/Moonscraper Chart Editor_Data"
     cp -r "Custom Resources" "$out/share/Custom Resources"
     cp -r "Config" "$out/share/Config"
