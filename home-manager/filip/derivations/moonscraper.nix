f48749d3535e972d8448ce509511cf8a28ef524c
BonnieToGamer <68508540+BonnieToGamer@users.noreply.github.com>
2024-06-11 22:28:20 +0200

    home-manager 2024-06-11 22:16 : id 226

--- home-manager/filip/derivations/moonscraper.nix
+++ home-manager/filip/derivations/moonscraper.nix
@@ -1,6 +1,6 @@
 { pkgs }:
 pkgs.stdenv.mkDerivation {
-	name = "notwaita-cursor";
+	name = "MoonscraperChartEditor";
 	src = pkgs.fetchurl {
 		url = "https://github.com/FireFox2000000/Moonscraper-Chart-Editor/releases/download/1.5.6/Moonscraper.Chart.Editor.v1.5.6.Linux.Universal.tar.gz";
 		sha256 = "01n9cqzbfl3ggl7ryxldlcs731542mr9ali14a8sx46j0yf76wbg";
