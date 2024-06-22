5b3216eedb1e8c549b455c89878f765da1699322
BonnieToGamer <68508540+BonnieToGamer@users.noreply.github.com>
2024-06-22 12:31:21 +0200

    191 current  2024-06-22 12:31:18  24.05.20240609.2819fff  6.6.32                          *

--- root/modules/desktop/hyprland.nix
+++ root/modules/desktop/hyprland.nix
@@ -1,9 +1,10 @@
-{ pkgs, ... }:
+{ pkgs, pkgs-unstable, ... }:
 {
 	environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
   programs.hyprland = {
     enable = true;
     xwayland.enable = true;
+    package = pkgs-unstable.hyprland;
   };
 }
